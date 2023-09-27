ed15e4295fbd59eda8b4dacd00d56af17114820f
Vinnn212 <145567193+Vinnn212@users.noreply.github.com>
2023-09-27 22:33:45 +0800

    Update Unfinished_minutes)convert.py

--- Unfinished_minutes)convert.py
+++ Unfinished_minutes)convert.py
@@ -1,4 +1,1 @@
-second = input("Enter an integer for seconds:")
-minutes = second / 60
-seconds = second % minutes
-print(second, "seconds is", round(minutes), minutes, "minutes and", seconds, "seconds")
+
